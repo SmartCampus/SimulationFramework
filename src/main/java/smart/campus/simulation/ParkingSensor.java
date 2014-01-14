b21498b39d9003a836222ee22d868a4d333e9373
Fabien Foerster <Fabien.FOERSTER@polytech.unice.fr>
2014-01-14 15:32:33 +0100

    PSIQJE-7 Création de la classe ParkingLot + liaison avec ParkingSensor

---
+++ src/main/java/smart/campus/simulation/ParkingSensor.java
@@ -0,0 +1,16 @@
+package smart.campus.simulation;
+
+import akka.actor.UntypedActor;
+
+/**
+ * Created by foerster on 14/01/14.
+ */
+public class ParkingSensor extends UntypedActor {
+
+
+
+    @Override
+    public void onReceive(Object o) throws Exception {
+
+    }
+}
