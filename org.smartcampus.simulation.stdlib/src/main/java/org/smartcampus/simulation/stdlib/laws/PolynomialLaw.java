afe95b8cfda3aaee5e443b1074b1ba036e0e14af
Jerome Rancati <Jerome.RANCATI@polytech.unice.fr>
2014-01-16 17:49:58 +0100

    PSIQJE-13: modification mineure de la loi polynomiale

--- org.smartcampus.simulation.stdlib/src/main/java/org/smartcampus/simulation/stdlib/laws/PolynomialLaw.java
+++ org.smartcampus.simulation.stdlib/src/main/java/org/smartcampus/simulation/stdlib/laws/PolynomialLaw.java
@@ -17,12 +17,9 @@ public class PolynomialLaw extends Law {
     
 
     /**
-     * create the PolynomialLaw
+     * create the PolynomialLaw.
+     * Each monomial has to be written even if it's null
      * @param doubles each coefficent of the polynomial sorting from the lower power to the higher one
-     * @param min the minimum of definition of the polynomial
-     * @param max the maximum of definition of the polynomial
-     * @param majoration the maximum point  Northing (ordonnee) of the function
-     * @param doubles
      */
     public PolynomialLaw(Double ...doubles) {
         this.coeficients = new LinkedList<Double>();
@@ -33,11 +30,33 @@ public class PolynomialLaw extends Law {
     
     @Override
     public double evalute(int x){
-        double res = this.coeficients.get(0);
-        for(int i=1;i<this.coeficients.size(); i++){
-            res+=this.coeficients.get(i)*Math.pow(x, i-1);
+        double res = 0;
+        for(int i=0;i<getCoeficients().size(); i++){
+            res+=getCoeficients().get(i)*Math.pow(x, i);
         }
         return res;
     }
+    
+    /**
+     * Getter for the original ordonnee
+     * @return the original ordonnee
+     */
+    private Double getOriginalOrdonee(){
+        return this.coeficients.get(0);
+    }
 
+    /**
+     * @return the coeficients
+     */
+    protected List<Double> getCoeficients() {
+        return coeficients;
+    }
+
+    /**
+     * @param coeficients the coeficients to set
+     */
+    protected void setCoeficients(List<Double> coeficients) {
+        this.coeficients = coeficients;
+    }
+    
 }
