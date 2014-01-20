f2450d30589227393bd3b8d18f21dc07e1f29ab3
Jerome Rancati <Jerome.RANCATI@polytech.unice.fr>
2014-01-20 09:54:39 +0100

    resolving error

--- org.smartcampus.simulation.stdlib/src/main/java/org/smartcampus/simulation/stdlib/simulationlaw/ParkingMarkovSimulationLaw.java
+++ org.smartcampus.simulation.stdlib/src/main/java/org/smartcampus/simulation/stdlib/simulationlaw/ParkingMarkovSimulationLaw.java
@@ -2,16 +2,24 @@ package org.smartcampus.simulation.stdlib.simulationlaw;
 
 import org.smartcampus.simulation.framework.simulator.SimulationLaw;
 
-public class ParkingMarkovSimulationLaw extends SimulationLaw<Integer, Double,Boolean> {
-	
-	@Override
-	protected Integer[] computeValue() {
-		int i = 0;
-		// counts the number of occupied parking spaces
-		for (boolean b : values){
-			if(b) i++;
-		}
-		Integer[] t= {i,i};
-		return t;
-	}
+public class ParkingMarkovSimulationLaw extends
+        SimulationLaw<Integer, Double, Boolean> {
+
+    @Override
+    protected Integer[] computeValue() {
+        int i = 0;
+        // counts the number of occupied parking spaces
+        for (boolean b : values) {
+            if (b)
+                i++;
+        }
+        Integer[] t = { i, i };
+        return t;
+    }
+
+    @Override
+    protected void onComplete() {
+        // TODO Auto-generated method stub
+
+    }
 }
