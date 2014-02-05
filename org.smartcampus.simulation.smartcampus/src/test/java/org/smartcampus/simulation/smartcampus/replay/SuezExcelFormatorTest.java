7be1408f3b79b8a4582da3f67c9ee1728f712447
Fabien Foerster <Fabien.FOERSTER@polytech.unice.fr>
2014-02-05 17:22:55 +0100

    PSIQJE-60 test de suezexcelformator

---
+++ org.smartcampus.simulation.smartcampus/src/test/java/org/smartcampus/simulation/smartcampus/replay/SuezExcelFormatorTest.java
@@ -0,0 +1,23 @@
+package org.smartcampus.simulation.smartcampus.replay;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * Created by foerster
+ *
+ * on 05/02/14.
+ *
+ */
+public class SuezExcelFormatorTest {
+
+    @Test
+    public void testTransform() throws Exception {
+        SuezExcelFormator formator = new SuezExcelFormator();
+        assertEquals(1391554807000L,formator.transform("02/05/2014 7"));
+    }
+
+
+}
