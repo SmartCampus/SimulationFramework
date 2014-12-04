8ff2f93456b40071214e15c7522d5f7e9e46f4dc
Romain Alexandre <alexandr@polytech.unice.fr>
2014-12-04 12:03:50 +0100

    Correction d'un bug de SuezExcelFormator

--- org.smartcampus.simulation.smartcampus/src/main/java/org/smartcampus/simulation/smartcampus/replay/SuezExcelFormator.java
+++ org.smartcampus.simulation.smartcampus/src/main/java/org/smartcampus/simulation/smartcampus/replay/SuezExcelFormator.java
@@ -4,32 +4,39 @@ import org.smartcampus.simulation.stdlib.replay.ExcelFormator;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by foerster
  * on 05/02/14.
  */
-public class SuezExcelFormator extends ExcelFormator{
+public class SuezExcelFormator extends ExcelFormator {
 
-    private final SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-    private final int numberOfMilliSecondsInADay = 86400000 ;
+    private final SimpleDateFormat sdf;
+    private final int numberOfMilliSecondsInADay;
 
-    public SuezExcelFormator(){
-        super(2,new String[]{"A","B"},2);
+    public SuezExcelFormator() {
+        super(2, new String[]{"A", "B"}, 2);
+
+        sdf = new SimpleDateFormat("MM/dd/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        numberOfMilliSecondsInADay = 86400000;
     }
 
     @Override
     protected long transform(String[] columns) {
-        long timestamp = 0 ;
+        long timestamp = 0;
         try {
-            timestamp = sdf.parse(columns[0]).getTime();
-            int hoursToMilli = Integer.valueOf(columns[1])*1000;
+            Date d =  sdf.parse(columns[0]);
+            d.setHours(0);
+            timestamp = d.getTime();
+            int hoursToMilli = Integer.valueOf(columns[1]) * 3600000;
             timestamp += hoursToMilli;
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
         return timestamp;
-
     }
 }
